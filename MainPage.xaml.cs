656b3312b5fdb6c4a53b2eae0c936849c5b7d015
DHRUV ITWALA <72596873+dhruv06122002@users.noreply.github.com>
2021-04-28 10:04:53 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Password_Generator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+          
+        }
+        
+        private void OnClick(object sender, EventArgs e)
+        {
+            int length = 7;
+
+            StringBuilder str_build = new StringBuilder();
+            Random random = new Random();
+
+            char letter;
+
+            for (int i = 0; i < length; i++)
+            {
+                double flt = random.NextDouble();
+                int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                letter = Convert.ToChar(shift + 65);
+                str_build.Append(letter);
+            }
+            this.resultText.Text = str_build.ToString();
+            
+        }
+    }   
+}
